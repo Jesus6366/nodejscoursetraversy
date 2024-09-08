f223167e7fd98537a3de883060a9922720f297a4
unknown <polibos6366@gmail.com>
2024-09-08 09:16:08 -0600

    simple http routing

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+import { error } from "console";
 import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.statusCode = 404;
+  // ROUTER
+  try {
+    // check if Get request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        // just in regular node.js
+        res.end("<h1>Home page</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        // just in regular node.js
+        res.end("<h1>About page</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        // just in regular node.js
+        res.end("<h1>Not found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    // just in regular node.js
+    res.end("Server error");
+  }
 
-  // this is the same
-  res.writeHead(200, { "Content-Type": "text/html" });
-
-  // just in regular node.js
-  res.end("<h1>Hello world</h1>");
+  // get the requested url
+  console.log(req.url);
+  // get the method used
+  console.log(req.method);
 });
 
 server.listen(PORT, () => {
