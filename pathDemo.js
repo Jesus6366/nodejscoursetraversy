b69e088ad6f649d9d1037dd0cc4f68c1018e9cae
unknown <polibos6366@gmail.com>
2024-09-08 11:52:57 -0600

    Path module

---
+++ pathDemo.js
@@ -0,0 +1,19 @@
+import path from "path";
+
+const filePath = "./dir1/dir2/test.txt";
+
+// basename()
+console.log(path.basename(filePath));
+
+// dirname()
+console.log(path.dirname(filePath));
+
+// extname()
+console.log(path.extname(filePath));
+
+// parse()
+console.log(path.parse(filePath));
+
+// join()
+// const filePath2 = path.join(__dirname, "dir1", "dir2", "test.txt");
+// console.log(filePath2);
